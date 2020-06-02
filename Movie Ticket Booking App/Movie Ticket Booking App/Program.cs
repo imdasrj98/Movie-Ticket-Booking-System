253c6cd34cb92610ae49768db63ecf9f521bd0a9
Debayan Das <dasriju98@gmail.com>
2020-06-02 21:34:28 +0530

    Demo SetUp Upadte1

--- Movie Ticket Booking App/Movie Ticket Booking App/Program.cs
+++ Movie Ticket Booking App/Movie Ticket Booking App/Program.cs
@@ -16,7 +16,7 @@ namespace Movie_Ticket_Booking_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
